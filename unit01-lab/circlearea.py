5425214b79b5e9dce01415ecb0cc3c423325ad24
rlowman <robertlowman@kings.edu>
2016-10-21 11:33:23 -0400

    adding finished python files

--- unit01-lab/circlearea.py
+++ unit01-lab/circlearea.py
@@ -3,10 +3,18 @@
 ## Name: Robert Lowman
 ## Date: 10/20/2016
 
+###############################################
+# computeArea() computes the area of the circle 
+#          with the given radius
+#
+# Param: radius, the radius of the circle
+# Return: the value of the area
 def computeArea(radius):
   PI = 3.1415927
-  pass PI * radius * radius
+  temp = (PI * radius * radius) 
+  return temp
 
+# Begin of execution
 r = input('To compute the area of a circle, enter its radius: ')
 result = computeArea(r)
-print('The area is ', result)
+print "The area is ", result
