203a8edcb595702f623df90150ed558a358f4323
rlowman <robertlowman@kings.edu>
2016-11-17 12:11:48 -0500

    adding downloaded files

---
+++ unit10-lab/scheme/max.scm
@@ -0,0 +1,35 @@
+;;; max.el searches a list for its maximum value.
+;;;
+;;; Begun by: Dr. Jump, CS 315 at King's College
+;;; Completed by:
+;;; Date:
+
+(load "./mylist.scm")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; test function Max. ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+(begin
+  (let ((listOne (list 99 88 77 66 55))
+        (listTwo (list 55 66 77 88 99))
+        (listThree (list 55 77 99 88 66)))
+    (display listOne)
+    (display "\n")
+    (display listTwo)
+    (display "\n")
+    (display listThree)
+    (display "\n")
+
+    (display "the max in listOne is ")
+    (display (max listOne))
+    (display "\n")
+
+    (display "the max in listTwo is ")
+    (display (max listTwo))
+    (display "\n")
+
+    (display "the max in listThree is ")
+    (display (max listThree))
+    (display "\n")
+
+    (quit)))
