7762ee45a99d11bf296918e25936440bf3a8c8c6
rlowman <robertlowman@kings.edu>
2016-12-06 09:57:53 -0500

    adding scheme file

--- unit11-hw/scheme/schemefunctions.scm
+++ unit11-hw/scheme/schemefunctions.scm
@@ -4,8 +4,33 @@
 ;;; Date: 12/5/16
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; 1a.
 (define (my-reverse aList)
-    (if (eqv? (length aList) 1)
-      (car aList)
-      (cons (my-reverse (cdr aList)) (car aList))
-      ))
+ (if (null? aList)
+     '()
+     (append (my-reverse (cdr aList)) (list (car aList)))
+  )
+)
+;; (my-reverse '(a b (c d) (e (f g))))
+;; Returns: ((e (f g)) (c d) b a)
+
+;; 1b.
+(define (super-reverse aList)
+    (define (super-reverse-helper aList returnList)
+      (if (null? aList)
+          returnList
+          (if (list? (car aList))
+              (super-reverse-helper (cdr aList) (cons (super-reverse (car aList)) returnList))
+              (super-reverse-helper (cdr aList) (cons (car aList) returnList)))))
+      (super-reverse-helper aList '()))
+;; (super-reverse '(a b (c d) (e (f g))))
+;; Returns: (((g f) e) (d c) b a)
+
+;; 2a.
+(define (mem value aList)
+  (if (null? aList)
+    'nil
+    (if (equal? value (car aList))
+        't
+        (mem value (cdr aList))
+        )))
